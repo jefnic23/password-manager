57a741c7b6361a34b6b8064111d0fbd03b9eab7b
Jeff <jefnic23@gmail.com>
2021-12-01 15:17:35 -0500

    decode secret key

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ from app.email import send_password_reset_email
 from app import app
 
 bootstrap = Bootstrap(app)
-fernet = Fernet(app.config['SECRET_KEY'].encode())
+fernet = Fernet(app.config['SECRET_KEY'].decode())
 login = LoginManager(app)
 login.init_app(app)
 
