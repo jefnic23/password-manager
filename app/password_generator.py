325c15e5ea43bf5470e898d0048edca0b852f305
Jeff <jefnic23@gmail.com>
2022-01-19 21:27:26 -0500

    password generator fix

--- app/password_generator.py
+++ app/password_generator.py
@@ -2,7 +2,8 @@ import string, secrets
 
 chars = [*string.ascii_letters,
          *string.digits,
-         *["-", "(", ")", ".", "&", "@", "?", "#", ",", "/", "+", "!"]]
+         *["!", "*", "@", "#", "$", "%", "&", "+", "="]]
+         
 
 def generate_password(chars=chars):
     length = secrets.SystemRandom().randrange(12, 16)
