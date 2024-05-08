ed56c487bfe1ef2c5de4e73f8d0b75577bedcbcf
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-07 23:09:57 -0400

    📝 (token.py): Add refresh_token field to Token schema 🔧 (auth_service.py): Update AuthService constructor to include AsyncSession parameter for database operations 🔧 (auth_service.py): Refactor generate_access_token method to generate_token with configurable expiry time 🔧 (auth_service.py): Refactor verify_password method to be a static method 🔧 (auth_service.py): Add verify_refresh_token and verify_token methods for token verification and database query operations

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,26 +1,33 @@
 from datetime import datetime, timedelta, timezone
 
 from config import Settings
-from jose import jwt
+from exceptions import credentials_exception
+from fastapi import HTTPException
+from jose import JWTError, jwt
 from models.user import User
 from passlib.context import CryptContext
 from services.users_service import UsersService
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from models.refresh_token import RefreshToken
 
 
 class AuthService:
     PASSWORD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    def __init__(self, users_service: UsersService, settings: Settings):
+    def __init__(
+        self, users_service: UsersService, settings: Settings, session: AsyncSession
+    ):
         self.users_service = users_service
         self.settings = settings
+        self.session = session
 
-    def generate_access_token(self, sub: str) -> str:
-        exp = datetime.now(timezone.utc) + timedelta(minutes=5)
+    def generate_token(self, expiry_minutes: int, sub: str) -> str:
+        exp = datetime.now(timezone.utc) + timedelta(minutes=expiry_minutes)
         claims = {
             "exp": exp,
             "sub": sub,
         }
-
         return jwt.encode(
             claims=claims,
             key=self.settings.SECRET_KEY,
@@ -31,9 +38,37 @@ class AuthService:
         user = await self.users_service.get_user(email=email)
         if not user:
             return False
-        if not self.verify_password(secret=password, hash=user.password):
+        if not AuthService.verify_password(secret=password, hash=user.password):
             return False
         return user
+
+    async def verify_refresh_token(
+        self, token: str, exception: HTTPException = credentials_exception
+    ) -> dict[str, any]:
+        payload = self.verify_token(token)
+        email = payload.get("sub")
+        exp = payload.get("exp")
+        statement = (
+            select(RefreshToken)
+            .where(RefreshToken.user.email == email)
+            .where(RefreshToken.expiry_time == exp)
+        )
+        results = await self.session.exec(statement=statement)
+        if not results.one_or_none():
+            raise exception
+        return payload
+
+    def verify_token(
+        self, token: str, exception: HTTPException = credentials_exception
+    ) -> dict[str, any]:
+        try:
+            payload = jwt.decode(token, self.settings.SECRET_KEY, algorithms=["HS256"])
+            email: str | None = payload.get("sub")
+            if email is None:
+                raise exception
+        except JWTError:
+            raise exception
+        return payload
 
     @staticmethod
     def verify_password(
