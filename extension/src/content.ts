2f8702e6e4838dcfdfb41dc4e737ebb8e3c6315e
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-09 22:45:40 -0400

    📝 (content.ts): Comment out unused code related to fetching and displaying password fields in the extension content script ♻️ (Popup.svelte): Refactor code to use named imports for components and stores, and improve readability by restructuring the logic in the onMount function

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -34,3 +34,94 @@ observer.observe(document.body, {
 // Remember to disconnect the observer when it's no longer needed to avoid memory leaks
 window.addEventListener('unload', () => observer.disconnect());
 
+// chrome.tabs.query(
+//     { active: true, currentWindow: true },
+//     async (tabs) => {
+//         let activeTab: chrome.tabs.Tab = tabs[0];
+
+//         const url: URL = new URL(activeTab.url as string);
+//         host = url.host;
+
+//         console.log(host);
+
+//         const response = await fetch(
+//             `http://127.0.0.1:8000/services/${host}`,
+//             {
+//                 headers: {
+//                     Authorization: `Bearer ${$accessToken}`,
+//                 },
+//             },
+//         );
+
+//         const data = await response.json();
+
+//         password = data;
+
+//         chrome.scripting.executeScript(
+//             {
+//                 target: { tabId: activeTab.id as number },
+//                 func: () => {
+//                     const inputs = document.querySelectorAll(
+//                         'input[type="password"]',
+//                     );
+//                     const hasPasswordInput = inputs.length > 0;
+
+//                     if (hasPasswordInput) {
+//                         const node = inputs[0];
+
+//                         if (node.ariaHidden) {
+//                             return false;
+//                         }
+
+//                         const container = document.createElement("div");
+//                         container.style.position = "relative";
+//                         container.style.width = "100%";
+//                         (node.parentNode as HTMLElement).insertBefore(
+//                             container,
+//                             node,
+//                         );
+//                         container.appendChild(node);
+
+//                         // Create the button
+//                         const button = document.createElement("button");
+//                         button.innerText = "🔑"; // Using an emoji as the button face
+//                         button.style.position = "absolute";
+//                         button.style.right = "0px";
+//                         button.style.top = "50%";
+//                         button.style.border = "none";
+//                         button.style.background = "transparent";
+//                         button.style.transform =
+//                             "translate(-50%, -50%)";
+//                         button.style.marginRight = "8px";
+//                         button.style.cursor = "pointer";
+//                         button.style.lineHeight = "1";
+
+//                         if (container.nextElementSibling?.innerHTML) {
+//                             button.style.paddingRight = "25px";
+//                         }
+
+//                         button.onclick = () => {
+//                             (node as HTMLInputElement).value =
+//                                 "YourSecurePassword"; // Set this to generate or fetch a secure password as needed
+//                         };
+
+//                         // Append the button to the container next to the input
+//                         container.appendChild(button);
+//                     }
+
+//                     return hasPasswordInput;
+//                 },
+//             },
+//             (results) => {
+//                 if (results && results[0]) {
+//                     console.log(
+//                         "Password input fields found:",
+//                         results[0].result,
+//                     );
+//                 } else {
+//                     console.log("No password input fields found.");
+//                 }
+//             },
+//         );
+//     },
+// );
--- extension/src/pages/Popup.svelte
+++ extension/src/pages/Popup.svelte
@@ -1,114 +1,39 @@
 <script lang="ts">
-	import Login from "../components/Login.svelte";
-	import { accessToken } from "../stores";
+	import { onMount } from "svelte";
+	import Login from "@components/Login.svelte";
+	import { jwtDecode } from "jwt-decode";
+	import { accessToken, refreshToken } from "@stores/tokens";
+	import { user, getUser } from "@stores/users";
 
-	let host: string | undefined;
-	let password: string | undefined;
-	let loggedIn: boolean = false;
+	onMount(async () => {
+		if ($accessToken && $refreshToken) {
+			const decodedAccessToken = jwtDecode($accessToken);
+			if (
+				(decodedAccessToken.exp as number) >
+				Math.floor(Date.now() / 1000)
+			) {
+				user.set(await getUser($accessToken));
+			}
+		}
+	});
 
-	$: if (loggedIn) {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			async (tabs) => {
-				let activeTab: chrome.tabs.Tab = tabs[0];
-
-				const url: URL = new URL(activeTab.url as string);
-				host = url.host;
-
-				console.log(host);
-
-				const response = await fetch(
-					`http://127.0.0.1:8000/services/${host}`,
-					{
-						headers: {
-							Authorization: `Bearer ${$accessToken}`,
-						},
-					},
-				);
-
-				const data = await response.json();
-
-				password = data;
-
-				// chrome.scripting.executeScript(
-				// 	{
-				// 		target: { tabId: activeTab.id as number },
-				// 		func: () => {
-				// 			const inputs = document.querySelectorAll(
-				// 				'input[type="password"]',
-				// 			);
-				// 			const hasPasswordInput = inputs.length > 0;
-
-				// 			if (hasPasswordInput) {
-				// 				const node = inputs[0];
-
-				// 				if (node.ariaHidden) {
-				// 					return false;
-				// 				}
-
-				// 				const container = document.createElement("div");
-				// 				container.style.position = "relative";
-				// 				container.style.width = "100%";
-				// 				(node.parentNode as HTMLElement).insertBefore(
-				// 					container,
-				// 					node,
-				// 				);
-				// 				container.appendChild(node);
-
-				// 				// Create the button
-				// 				const button = document.createElement("button");
-				// 				button.innerText = "🔑"; // Using an emoji as the button face
-				// 				button.style.position = "absolute";
-				// 				button.style.right = "0px";
-				// 				button.style.top = "50%";
-				// 				button.style.border = "none";
-				// 				button.style.background = "transparent";
-				// 				button.style.transform = "translate(-50%, -50%)";
-				// 				button.style.marginRight = "8px";
-				// 				button.style.cursor = "pointer";
-				// 				button.style.lineHeight = "1";
-
-				// 				if (container.nextElementSibling?.innerHTML) {
-				// 					button.style.paddingRight = "25px";
-				// 				}
-
-				// 				button.onclick = () => {
-				// 					(node as HTMLInputElement).value =
-				// 						"YourSecurePassword"; // Set this to generate or fetch a secure password as needed
-				// 				};
-
-				// 				// Append the button to the container next to the input
-				// 				container.appendChild(button);
-				// 			}
-
-				// 			return hasPasswordInput;
-				// 		},
-				// 	},
-				// 	(results) => {
-				// 		if (results && results[0]) {
-				// 			console.log(
-				// 				"Password input fields found:",
-				// 				results[0].result,
-				// 			);
-				// 		} else {
-				// 			console.log("No password input fields found.");
-				// 		}
-				// 	},
-				// );
-			},
-		);
-	}
+	// on mount, check for access and refresh tokens
+	// if access token, attempt to get user
+	// else, show login
+	// if user attempt successful, show user
+	// else, attempt refresh
+	// if refresh successful, get user
+	// else, show login screen
 </script>
 
 <div>
 	<img src="/icon.png" alt="" />
-	{#if !loggedIn}
-		<Login bind:loggedIn />
-	{:else}
+	{#if $user}
 		<p>
-			Current page: <code>{host}</code>
-			Password: <code>{password}</code>
+			Welcome, {$user}
 		</p>
+	{:else}
+		<Login />
 	{/if}
 </div>
 
